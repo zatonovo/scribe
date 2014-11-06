51bc0afd246266f03ec42848af0579197d6b5f7d
Brian Lee Yung Rowe <rowe@nurometic.com>
2014-11-06 12:42:06 -0500

    Properly handle timestamps

--- src/scribe_util.erl
+++ src/scribe_util.erl
@@ -1,10 +1,17 @@
 -module(scribe_util).
--export([to_lower/1, to_map/1, to_map/2, to_ejson/1, to_ejson/2]).
+-export([is_string/1, to_lower/1,
+  to_map/1, to_map/2, to_ejson/1, to_ejson/2]).
 -export([term_parser/1]).
 
+is_string(List) ->
+  Ascii = io_lib:printable_list(List),
+  Unicode = io_lib:printable_unicode_list(List),
+  Ascii or Unicode.
+  
 to_lower(BitString) ->
   String = bitstring_to_list(BitString),
   list_to_bitstring(string:to_lower(String)).
+
 
 to_map(O) -> to_map(O, fun term_parser/1).
 
@@ -19,7 +26,7 @@ to_map([H|T], TermParser) ->
   case H of
     {_,_} -> lists:foldl(Fn, #{}, [H|T]);
     [{_,_}|_] -> lists:map(fun to_map/1, [H|T]);
-    _ -> [H|T]
+    _ -> TermParser([H|T])
   end.
 
 
@@ -36,13 +43,23 @@ to_ejson([H|T], TermParser) ->
   case H of
     {_,_} -> {lists:map(Fn, [H|T])};
     [{_,_}|_] -> lists:map(fun to_ejson/1, [H|T]);
-    _ -> [H|T]
+    _ -> TermParser([H|T])
   end.
 
 
 term_parser({{Y,M,D},{H,N,S}}) ->
-  Date = string:join([integer_to_list(Y),integer_to_list(M),integer_to_list(D)], "-"),
-  Time = string:join([integer_to_list(H),integer_to_list(N),integer_to_list(S)], ":"),
-  string:join([Date,Time], "T");
+  F4 = fun integer_to_list/1,
+  F = fun(X) ->
+    string:right(integer_to_list(X),2,$0)
+  end,
+  Date = list_to_binary(string:join([F4(Y),F(M),F(D)], "-")),
+  Time = list_to_binary(string:join([F(H),F(N),F(S)], ":")),
+  <<Date/binary,"T",Time/binary>>;
+
+term_parser(Term) when is_list(Term) ->
+  case is_string(Term) of
+    true -> list_to_binary(Term);
+    false -> Term
+  end;
 
 term_parser(Term) -> Term.
